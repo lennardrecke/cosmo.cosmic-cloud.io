1a7ec72138c5022a0ff1334db6409bcd04b8cc25
stlerecke <lennard.recke@stud.h-da.de>
2023-07-28 19:23:11 +0200

    working conversation model

---
+++ components/empty.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+interface EmptyProps {
+  label: string;
+}
+
+export const Empty = ({ label }: EmptyProps) => {
+  return (
+    <div className="h-full p-20 flex flex-col items-center justify-center">
+      <div className="relative h-64 aspect-square">
+        <Image
+          alt="Empty"
+          fill
+          src="/empty.png"
+          className="rounded-2xl shadow-md"
+        />
+      </div>
+      <p className="text-muted-foreground text-lg text-center mt-2">{label}</p>
+    </div>
+  );
+};
